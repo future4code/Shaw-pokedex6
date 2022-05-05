8d4086a003d5dfcdfb15c77658452981396ddd54
Renan Martinez <remam_martinez89@hotmail.com>
2022-05-05 10:41:38 -0300

    passado array pokedex com globalcontext

--- pokedex/src/hooks/useRequestData.js
+++ pokedex/src/hooks/useRequestData.js
@@ -1,13 +1,14 @@
 import { useEffect, useState } from 'react'
 import axios from "axios";
 
+
 const useRequestData = (url) => {
   const [data, setData] = useState();
-
+ 
 
   useEffect(() => {
      axios
-      .get("https://pokeapi.co/api/v2/pokemon/")
+      .get(url)
       .then((res) => {
         setData(res.data.results);
       })
